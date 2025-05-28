825d1442daf7b2428669caaaf62545981ebfd441
Matt James Benson <mattbenson04@googlemail.com>
2025-05-28 21:25:36 +0100

    Authentication working

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoListController;
 use App\Http\Controllers\ListItemController;
 
-Route::resource('todo-lists', TodoListController::class);
-Route::resource('todo-lists.items', ListItemController::class);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: [
-        'resources/scss/front.scss',
-        'resources/js/front.js',
-      ],
-      refresh: true,
-    }),
-    tailwindcss(),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "bootstrap/scss/bootstrap";`,
-      },
-    },
-  },
-  server: {
-    hmr: {
-      host: 'vetorg.test',
-      watch: {
-        usePolling: true,
-      },
-    },
-  },
+    plugins: [
+        laravel({
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
 });
